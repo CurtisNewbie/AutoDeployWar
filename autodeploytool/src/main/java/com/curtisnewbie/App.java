1aa884d2ae9330e166efcac3658c0fa4ccab9f75
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-16 14:14:46 +0000

    Remove unused imports

--- autodeploytool/src/main/java/com/curtisnewbie/App.java
+++ autodeploytool/src/main/java/com/curtisnewbie/App.java
@@ -5,11 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
 
